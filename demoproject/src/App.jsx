6f01c60a0b52778f4e76fa6ab3828ac96ab34503
Fayekuzzaman Rizvi <frrizvi11@gmail.com>
2025-04-13 21:00:49 +0600

    demo

---
+++ demoproject/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Navber from './component/Navber';
+import Hero from './component/Hero';
+
+const App = () => {
+  return (
+    <div>
+      <Navber />
+      <Hero />
+
+    </div>
+  );
+};
+
+export default App;
