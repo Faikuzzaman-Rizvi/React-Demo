90b8bed8c5b9f8c4a737841e7a6effeb31d26832
Fayekuzzaman Rizvi <frrizvi11@gmail.com>
2025-04-14 19:26:55 +0600

    result check

--- demoproject/src/App.jsx
+++ demoproject/src/App.jsx
@@ -1,12 +1,36 @@
 
 import Navber from './component/Navber';
 import Hero from './component/Hero';
+import { useState } from 'react';
 
 const App = () => {
+
+  const [number,setNumber]=useState()
+
+  let marks =number;
+
   return (
     <div>
-      <Navber />
-      <Hero />
+
+    <form action="">
+          <input type="text" onChange={(e)=>setNumber(e.target.value)} />
+          <button>check result</button>
+    </form>
+
+    {( () =>{
+
+      if(marks>80 && marks<100){
+        return <h1>A+</h1>
+      }
+      else if(marks>70 && marks<80){
+        return <h1>A</h1>
+      }
+      else{
+        return <h1>Youe are not Allow!</h1>
+      }
+        
+
+    }) ()}
 
     </div>
   );
